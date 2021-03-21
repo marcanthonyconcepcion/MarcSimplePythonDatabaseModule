6658570adcb9a3a7d9a52e058cd7fedaab294dcb
Marc Concepcion <marcanthonyconcepcion@gmail.com>
2021-03-22 01:58:35 +0800

    Initial Commit

---
+++ database_records.py
@@ -0,0 +1,60 @@
+from typing import Final
+import yaml
+import mysql.connector
+
+with open(r'resources\database_demo.yaml') as file:
+    configuration = yaml.full_load(file)
+
+HOST: Final = configuration['database']['host']
+DBNAME: Final = configuration['database']['dbname']
+USER: Final = configuration['database']['user']
+PASSWORD: Final = configuration['database']['password']
+
+
+class DatabaseRecords:
+
+    def __init__(self):
+        self.database = mysql.connector.connect(host=HOST, database=DBNAME,
+                                                user=USER, password=PASSWORD)
+        self.cursor = self.database.cursor()
+
+    def disconnect(self):
+        self.database.disconnect()
+
+    def fetch(self, query, parameters=None):
+        try:
+            self.cursor.execute(operation=query, params=parameters)
+            for result in self.cursor:
+                yield result
+        finally:
+            self.cursor.reset()
+
+    def edit(self, query, parameters=None):
+        try:
+            self.cursor.execute(operation=query, params=parameters)
+            self.database.commit()
+        except Exception as exception:
+            self.database.rollback()
+            raise DatabaseTransactionError(
+                'ERROR updating records to the database. Rolled back changes. {details}'.format(details=exception))
+
+    def bulk_edit(self, query, parameter_sets):
+        try:
+            self.cursor.executemany(operation=query, seq_params=parameter_sets)
+            self.database.commit()
+        except Exception as exception:
+            self.database.rollback()
+            raise DatabaseTransactionError(
+                'ERROR creating new records to the database. Rolled back changes. {details}'.format(details=exception))
+
+    __instance__ = None
+
+    @staticmethod
+    def get():
+        if DatabaseRecords.__instance__ is None:
+            DatabaseRecords.__instance__ = DatabaseRecords()
+        return DatabaseRecords.__instance__
+
+
+class DatabaseTransactionError(Exception):
+    pass
---
+++ tests/test_database_records.py
@@ -0,0 +1,62 @@
+import unittest
+from decimal import Decimal
+
+import database_records
+from database_records import DatabaseRecords
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.expected_configuration = {'database': {'host': 'localhost', 'dbname': 'demo_database',
+                                                    'user': 'user', 'password': 'password'}}
+        self.expected_records = [{'index': 1, 'name': 'apple', 'weight': Decimal('200.03'), 'price': Decimal('40.05')},
+                                 {'index': 2, 'name': 'banana', 'weight': Decimal('400.03'), 'price': Decimal('80.05')},
+                                 {'index': 3, 'name': 'orange', 'weight': Decimal('193.57'), 'price': Decimal('45.75')},
+                                 {'index': 4, 'name': 'grape', 'weight': Decimal('393.43'), 'price': Decimal('100.28')}]
+        self.dut = DatabaseRecords.get()
+        self.dut.bulk_edit('insert into `fruits` (`name`, `weight`, `price`) values (%(name)s, %(weight)s,%(price)s)',
+                           [{key: record[key] for key in record if key is not 'index'}
+                            for record in self.expected_records])
+
+    def tearDown(self):
+        self.dut.edit('truncate table `fruits`')
+
+    def test_config_in_database(self):
+        self.assertEqual(database_records.HOST, self.expected_configuration['database']['host'])
+        self.assertEqual(database_records.DBNAME, self.expected_configuration['database']['dbname'])
+        self.assertEqual(database_records.USER, self.expected_configuration['database']['user'])
+        self.assertEqual(database_records.USER, self.expected_configuration['database']['password'])
+
+    def test_connection(self):
+        self.assertEqual(1, next(self.dut.fetch('select %(number)s', {'number': 1}))[0])
+
+    def test_retrieve(self):
+        expected_records = [tuple(record.values()) for record in self.expected_records]
+        records = self.dut.fetch('select  * from `fruits`')
+        index = 0
+        for record in records:
+            self.assertEqual(expected_records[index], record)
+            index += 1
+        self.assertEqual(len(expected_records), index)
+
+    def test_update(self):
+        self.expected_records[1 - 1]['name'] = 'Mandarin'
+        self.dut.edit('update `fruits` set `name`= %(name)s where `index`= %(index)s',
+                      {'name': 'Mandarin', 'index': 1})
+        self.test_retrieve()
+
+    def test_delete(self):
+        del self.expected_records[3 - 1]
+        self.dut.edit('delete from `fruits` where `name` = %(name)s', {'name': 'orange'})
+        self.test_retrieve()
+
+    def test_single_insert(self):
+        new_record = {'index': 5, 'name': 'watermelon', 'weight': Decimal('555.55'), 'price': Decimal('70.00')}
+        self.expected_records.append(new_record)
+        self.dut.edit('insert into `fruits` (`name`, `weight`, `price`) values (%(name)s, %(weight)s,%(price)s)',
+                      {key: new_record[key] for key in new_record if key is not 'index'})
+        self.test_retrieve()
+
+
+if __name__ == '__main__':
+    unittest.main()
